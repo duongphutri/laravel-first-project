badcdaf07b0c6c4b58e8d7f683cb1de7578762ce
duongphutri <duongphutri@gmail.com>
2021-06-21 11:08:32 +0700

    layout

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,51 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends ('layouts.app') @section('content')
+<a class="btn btn-primary" href="{{route('post.create')}}">create</a>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Document</title>
-</head>
+<table class="table table-light">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">title</th>
+            <th scope="col">name</th>
+            <th scope="col">created</th>
+            <th scope="col">action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($posts as $post)
+        <tr>
+            <th scope="row">
+                {{ $loop->iteration }}
+            </th>
+            <td>{{$post->title}}</td>
+            <td>{{$post->name}}</td>
+            <td>{{$post->created_by}}</td>
+            <td>
+                <a href="{{ route('post.show', ['post'=> $post]) }}">view</a>
+                <a href="{{ route('post.edit',['post'=>$post])}}">edit</a>
+                <a href="{{ route('post.destroy',['post'=>$post])}}">delete</a>
 
-<body>
-    <a class="btn btn-primary" href="{{route('post.create')}}">create</a>
-
-    <table class="table table-light">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">title</th>
-                <th scope="col">name</th>
-                <th scope="col">created</th>
-                <th scope="col">action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($posts as $post)
-            <tr>
-                <th scope="row">
-                    {{ $loop->iteration }}
-                </th>
-                <td>{{$post->title}}</td>
-                <td>{{$post->name}}</td>
-                <td>{{$post->created_by}}</td>
-                <td>
-                    <a href="{{ route('post.show', ['post'=> $post]) }}">view</a>
-                    <a href="{{ route('post.edit',['post'=>$post])}}">edit</a>
-                    <a href="{{ route('post.destroy',['post'=>$post])}}">delete</a>
-
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-
-</html>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
